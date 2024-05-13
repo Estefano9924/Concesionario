6350c68c44f538f87800e9d449b1c8ea8907eea8
SebasAlvarez15 <sebastian52221@gmail.com>
2024-05-13 12:11:21 -0500

    obtainVechicle y payload

--- context/firebase/firebaseState.js
+++ context/firebase/firebaseState.js
@@ -2,20 +2,51 @@ import React,{useReducer} from "react";
 import firebase from "../../firebase";
 import FirebaseReducer from "./firebaseReducer";
 import FirebaseContext from "./firebaseContext";
+import _ from 'lodash'
+import { getVehicle } from "../../type";
 
 const FirebaseState = props =>{
-    //Crea ek state inicial
+    //Crea el state inicial
     const initialState ={
         vehiculo:[]
     }
 
     //userReducer con el dispatch
     const [state, dispatch]= useReducer(FirebaseReducer,initialState)
-    
+    //traer los datos
+    const obtainVihicle = () =>{
+        //consulta a la bd
+        firebase.db
+        .collection('vehicle')//asi se llama la coleccion en firebase
+        .onSnapshot(manejarSnapshot)//manejo de la base de datos en tiempo real
+       
+        function manejarSnapshot (snapshot){
+            //desestructurar objeto
+            let car = snapshot.docs.map(doc =>{
+                return{
+                    id: doc.id,
+                    ...doc.data()
+                }
+            });
+            //Organizar informacion consultada lodahs
+            car = _.sortBy(car,'categoriaScrollView')
+            console.log(car) 
+            //el dispatch es del useReducer
+            dispatch({
+                type: getVehicle,
+                payload: car
+            })
+        }
+    }
     return(
 
         <FirebaseContext.Provider
-        value ={{vehiculo: state.vehiculo, firebase}}
+        value ={{
+            vehiculo: 
+            state.vehiculo,
+            firebase,
+            obtainVihicle
+        }}
         >
             {props.children}
         </FirebaseContext.Provider>
---
+++ type/index.js
@@ -0,0 +1,3 @@
+//Manejo de los estados por componente
+export const getVehicle = 'getVehicle'
+export const selectVehicle = 'selectVehicle'
